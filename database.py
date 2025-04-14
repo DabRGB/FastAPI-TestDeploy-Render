a58493afd88ae25a030f5ee8dcfef37d73706978
DabRGB <clavite.dave09@gmail.com>
2025-04-14 17:46:45 +0800

    Second attemp

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-database_url = os.environ.get("postgresql://todolist_django_render_pqsu_user:0WNrhPMDSLxLPkX65wUXUwH266NARXx5@dpg-cvnu4mruibrs73aeldg0-a.singapore-postgres.render.com/todolist_django_render_pqsu")
+# Get database URL from environment variable and ensure proper format
+database_url = os.environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://")
 
 engine = create_engine(database_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
